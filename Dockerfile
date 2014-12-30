ec506c1e8198567fd46583f01af75b0b2638eaf3
Cellophan <githubcom.cell@outer.systems>
2014-12-30 21:39:47 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM debian:stable
+
+RUN apt-get update; apt-get install -qy python g++ make checkinstall fakeroot wget
+RUN src=$(mktemp -d) && cd $src ;\
+  wget -N http://nodejs.org/dist/node-latest.tar.gz ;\
+  tar xzvf node-latest.tar.gz && cd node-v* ;\
+  ./configure ;\
+  fakeroot checkinstall -y --install=no --pkgversion $(echo $(pwd) | sed -n -re's/.+node-v(.+)$/\1/p') make -j$(($(nproc)+1)) install ;\
+  dpkg -i node_*
+RUN npm install -g native-dns
+
+RUN apt-get install -qy curl
+RUN curl -sSL https://get.docker.com/ | sh
+
+#https://github.com/tjfontaine/node-dns
+
---
+++ docker-dns.js
@@ -0,0 +1,29 @@
+
+var dns = require('native-dns');
+var util = require('util');
+var exec = require('child_process').exec;
+
+var server = dns.createServer();
+
+server.on('request', function (request, response) {
+	domain = request.question[0].name
+	regex = /([-_.a-z]*).docker.?/i;
+	name = domain.match(regex);
+	if (name) {
+		exec("docker inspect -f '{{.NetworkSettings.IPAddress}}' " + name[1].trim(), function (error, stdout, stderr) {
+			if (stdout) {
+				response.answer.push(dns.A(
+					{ name: domain, address: stdout.trim(), ttl: 30 }
+				)) }
+			response.send(); });
+	} else {
+		response.send(); }
+});
+
+server.on('error', function (err, buff, req, res) {
+  console.log(err.stack);
+});
+
+console.log('Listening on ' + 53);
+server.serve(53);
+
